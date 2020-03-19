b27e9c2b1599d8d0244aafa6b7813824144ab87f
Michael Banck <mbanck@debian.org>
2020-03-19 09:25:00 +0100

    Fix tab vs. whitespace

--- patroni/ctl.py
+++ patroni/ctl.py
@@ -1232,7 +1232,7 @@ def cluster_health(obj, cluster_name):
             click.echo('cluster is healthy')
             return
         else:
-	    click.echo('cluster has leader but is not healthy')
+            click.echo('cluster has leader but is not healthy')
             sys.exit(1)
     click.echo('cluster is not healthy')
     sys.exit(2)
